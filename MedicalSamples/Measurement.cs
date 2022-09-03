7f19c7355d1869120a851bbd2f9339a7ed19235f
domicell <remrita@gmail.com>
2022-09-03 05:54:47 +0300

    Add project files.

---
+++ MedicalSamples/Measurement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalSamples
+{
+    enum MeasurementType
+    {
+        Temperature,
+        HeartRate,
+        SpO2
+    }
+    class Measurement
+    {
+        private DateTime measurementTime;
+        private Double measurementValue;
+        private MeasurementType type;
+    }
+}
---
+++ MedicalSamples/Measurements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalSamples
+{
+    class Measurements
+    {
+        private DateTime measurementTime;
+        private Double measurementValue;
+        private MeasurementType type;
+        public Dictionary<MeasurementType, List<Measurement>> sample(
+        DateTime startOfSampling, List<Measurement> unsampledMeasurements)
+        {
+
+            return null;
+        }
+    }
+}
